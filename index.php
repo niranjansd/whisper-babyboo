3ac3a470be667c4655047cdba5be6fe79bd658fc
niranjansd <niranjan.sd@gmail.com>
2023-03-26 12:58:05 -0700

    working app with a transcript field.

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
   </head>
   <body>
     <button id="recordButton">Record</button>
+    <div id="loader">
+      <div class="spinner"></div>
+    </div>    
     <!-- Make a box to show transcript -->
-    <div id="transcript"></div>
+    <div id="transcript" contenteditable="true" data-placeholder="What you said"></div>
     <!-- <script src="js/layer.min.js" type="application/javascript"></script> -->
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery.cookie.min.js"></script>
--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 const recordButton = document.getElementById('recordButton');
+const loader = document.getElementById('loader');
 let recorder = null;
+
+// For todays date;
+Date.prototype.today = function () { 
+  return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+// For the time now
+Date.prototype.timeNow = function () {
+   return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
+function myFunction() {
+  // your code to run after the timeout
+}
+
+function checkExistsWithTimeout(path, timeout) {
+  return new Promise((resolve, reject) => {
+    const timeoutTimerId = setTimeout(handleTimeout, timeout)
+    const interval = timeout / 6
+    let intervalTimerId
+
+    function handleTimeout() {
+      clearTimeout(timerId)
+
+      const error = new Error('path check timed out')
+      error.name = 'PATH_CHECK_TIMED_OUT'
+      reject(error)
+    }
+
+    function handleInterval() {
+      fs.access(path, (err) => {
+        if(err) {
+          intervalTimerId = setTimeout(handleInterval, interval)
+        } else {
+          clearTimeout(timeoutTimerId)
+          resolve(path)
+        }
+      })
+    }
+
+    intervalTimerId = setTimeout(handleInterval, interval)
+  })
+}
 
 recordButton.addEventListener('click', () => {
   if (recorder === null) {
@@ -12,22 +56,22 @@ recordButton.addEventListener('click', () => {
       });
   } else {
     recorder.stop();
+    var datetime = new Date().today() + "_" + new Date().timeNow();
+    var datetime = datetime.replace(/:/g, "-").replace("/", "-").replace("/", "-");
+    var filename = 'recording_'+datetime+'.m4a'
     recorder.addEventListener('dataavailable', (event) => {
-        const audioFile = new Blob([event.data], { type: 'audio/wav' });
+        const audioFile = new Blob([event.data], { type: 'audio/mp4' });
         const url = URL.createObjectURL(audioFile);
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'recording.wav';
+        a.download = filename;
         a.click();
         console.log('recording saved');
+        loader.style.display = 'block';
+        send_prompt("C:\\Users\\niran\\Downloads\\"+filename);
+        loader.style.display = 'none';
       });
     recordButton.textContent = 'Record';
-    // Show loading animation untile next command is executed.
-    var loading = layer.load(1, {
-      shade: [0.1, '#fff'] //0.1透明度的白色背景
-    });
-    send_prompt("C:\\Users\\niran\\Downloads\\KajuAudio\\Papi - Wednesday at 5-30 PM\\Papi - Wednesday at 5-30 PM.m4a");
-
   }
 });
 
@@ -35,9 +79,10 @@ recordButton.addEventListener('click', () => {
 function send_prompt(filename) {
 
     if (filename == "") {
-        console.log("Please ask your question.");
+        console.log("No file detected.");
         return;
     };
+    console.log("Sending file: " + filename);
     req = $.ajax({
         cache: true,
         type: "POST",
